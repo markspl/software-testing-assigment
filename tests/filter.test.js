34584cb08e92de211c7eaec0fdc1a7bb07f6051d
Antti Eskelinen <antti.eskelinen3@gmail.com>
2020-11-21 10:59:40 +0200

    Added filter tests

---
+++ tests/filter.test.js
@@ -0,0 +1,32 @@
+import filter from "../source/src/filter";
+
+describe("Test filter", () => {
+  test("with empty array", () => {
+    expect(filter([], () => true)).toEqual([[]]);
+  });
+
+  test("with always true predicate", () => {
+    expect(filter([{}, {}], () => true)).toEqual([{}, {}]);
+  });
+
+  test("with always false predicate", () => {
+    expect(filter([1, 2], () => false)).toEqual([]);
+  });
+
+  test("even numbers", () => {
+    expect(filter([1, 2], () => false)).toEqual([2]);
+  });
+
+  // Non planned test to validate suspicion
+  test("list of objects", () => {
+    expect(
+      filter(
+        [
+          { id: 1, b: true },
+          { id: 2, b: false },
+        ],
+        ({ b }) => b
+      )
+    ).toEqual([{ id: 1, b: true }]);
+  });
+});
--- tests/helloWorld.test.js
+++
@@ -1,3 +0,0 @@
-test('sample test for ci config', () => {
-    expect(1+1+1).toBe(3);
-})
